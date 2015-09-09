65c361b8e47df63e3b3f1364fa8070b4c5a4e780
Radifan <radifan.aditya@gmail.com>
2015-09-09 17:26:04 +0700

    Functional test now checks we can input a to-do item

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+# def home_page(request):
+    # return HttpResponse('<html><title>Mahasiswa PMPL</title><p>Nama : Radifan Aditya R</p><p>NPM : 1206238293</p></html>')
+
 def home_page(request):
-    return HttpResponse('<html><title>Mahasiswa PMPL</title><p>Nama : Radifan Aditya R</p><p>NPM : 1206238293</p></html>')
+    return render(request, 'home.html')
