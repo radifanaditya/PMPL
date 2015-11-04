90b0e5937dc7290c1ea9b1343f9b461851108ca1
Radifan <radifan.aditya@gmail.com>
2015-11-04 19:03:46 +0700

    Edit static BASE_DIR destination in settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -118,7 +118,7 @@ ALLOWED_HOSTS = ['*']
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../../static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'lists/static'),
